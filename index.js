62afe37c3a730c8b293576080a67113c9e4be992
tiffanystud <tiffanylarsson.stud@gmail.com>
2024-11-21 16:54:41 +0100

    Initialized a function
    Function createDistanceTable.
    To create the distance table.

--- index.js
+++ index.js
@@ -8,6 +8,45 @@ function createAllCityBoxes(city) {
     return cityDiv;
 }
 
+function markCityBox(cityObject, kindOfCity) {
+    const cityBoxes = document.querySelectorAll(".cityBox");
+    for (let cityBox of cityObject) {
+        if (cityBox.textContent == cityObject.name) {
+            cityBox.classList.add("target");
+        } else if (cityBox > 1) { //Uppdatera villkoret
+            cityBox.classList.add("closest");
+        } else {
+            cityBox.classList.add("furthest");
+        }
+    }
+}
+
+
+
+
+//--------------------------------------------------
+//--------------------------------------------------
+function createDistanceTable(arrayElems) {
+
+    //Interera över alla element i distances, lägg till alla unika städer (dvs. kontroll av dubbletter)
+    const cityIds = [];
+    for (let i = 0; i < distances.length; i++) {
+        if (!cityIds.includes(distances[i].city1)) {
+            cityIds.push(distances[i].city1);
+        }; 
+        if (!cityIds.includes(distances[i].city2)) {
+            cityIds.push(distances[i].city2);
+        }; 
+    };
+
+
+}; //Fyll udda rutor (med modulus??)
+//--------------------------------------------------
+//--------------------------------------------------
+
+
+
+
 
 
 function isCityFound(target) {
@@ -22,7 +61,7 @@ function isCityFound(target) {
         if (targetLC == cityValueLC) {
             //Jämför target med indexerad (chosenCity) array i Cities
             cityFound = true;
-            h2.textContent = target + ' (' + cityValue.country + ')';
+            h2.textContent = target + " (" + cityValue.country + ")";
             chosenCity = cityValue;
             //Sätt rätt title (fliken)
             titleElem.innerText = cityValue.name;
@@ -44,19 +83,23 @@ const h3 = document.querySelector("h3");
 const closestCity = document.getElementById("closest");
 const furthestCity = document.getElementById("furthest");
 const divCities = document.getElementById("cities");
+const tableElem = document.getElementById("table");
 
 
 // Recommended: Ask for the city name and then the rest of the code
-
 const target = prompt("Vilken stad?");
-let divCitiesElem = document.getElementById("cities");
 
+
+let divCitiesElem = document.getElementById("cities");
 //Anrop av funktionen "createAllCityBoxes" för att skapa divar för varje stad.
 for (let city of cities) {
     divCitiesElem.append(createAllCityBoxes(city));
 };
 
-
-
 const cityMatched = isCityFound(target);
-console.log(cityMatched);
+
+
+// Interering av distances i databasen, det görs ett funktionsanrop per element/object i distances arrayen. 
+for (let arrayElems of distances) {
+    createDistanceTable(arrayElems);
+}
